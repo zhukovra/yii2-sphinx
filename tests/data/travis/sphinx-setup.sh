5aaa68ece77504548641bf1b64a5367a70d85f1a
Zhukov Roman <zhukov.roman@gmail.com>
2018-02-07 00:49:27 +0300

    Upgrade Sphinx to 3.0.1

--- tests/data/travis/sphinx-setup.sh
+++ tests/data/travis/sphinx-setup.sh
@@ -2,9 +2,17 @@
 SCRIPT=$(readlink -f "$0")
 CWD=$(dirname "$SCRIPT")
 
-# install sphinx from http://sphinxsearch.com/downloads/release/
-wget http://sphinxsearch.com/files/sphinxsearch_2.2.11-release-1~trusty_amd64.deb
-sudo dpkg -i sphinxsearch_2.2.11-release-1~trusty_amd64.deb
+if [ "$SPHINX_VERSION" = "3" ]; then
+    # download and extract sphinx
+    DISTR=sphinx-3.0.1-7fec4f6-linux-amd64
+    wget http://sphinxsearch.com/files/$DISTR.tar.gz
+    tar xf $DISTR.tar.gz
+    sudo cp $DISTR/bin/* /usr/bin
+else
+    # install sphinx from http://sphinxsearch.com/downloads/release/
+    wget http://sphinxsearch.com/files/sphinxsearch_2.2.11-release-1~trusty_amd64.deb
+    sudo dpkg -i sphinxsearch_2.2.11-release-1~trusty_amd64.deb
+fi
 
 # make dir that is used in sphinx config
 mkdir -p sphinx
